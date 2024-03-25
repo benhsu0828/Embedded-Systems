d3815c65290ae8009ae952f9ea130d0b50bdc567
williamimmanuelabadi <74845762+williamimmanuelabadi@users.noreply.github.com>
2024-03-25 10:51:57 +0800

    Add files via upload

--- Inclass/pyserial.py
+++ Inclass/pyserial.py
@@ -1,13 +1,27 @@
 import serial
-COM_PORT='COM3'
+import time
+import re
+COM_PORT='COM7'
 BAUD_RATES=115200
 ser=serial.Serial(COM_PORT,BAUD_RATES)
 try:
     while True:
         while ser.in_waiting:
-            msg=ser.readline().decode()
-            print('Serial_msg:{}'.format(msg))
-            
+            try:
+                msg=ser.readline().decode()
+                print('serial_msg: ',msg)
+                msg_data=re.split(',|:',msg)
+                
+                if("攝氏溫度" in str(msg_data)) and ("華氏溫度" in str(msg_data)) and ('相對溼度' in str(msg_data)):
+                    print('msg_data='+str(msg_data))
+                    print('msg_data[0]='+str(msg_data[1]))
+                else:
+                    continue
+                time.sleep(1)
+            except OSError as er:
+                ser.close()
+                print(er)
+                
 except KeyboardInterrupt:
     ser.close()
     print('closed!')
