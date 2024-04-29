b797678bef9897173c1a5cd8e997607a3603b62c
benhsu0828 <97419397+benhsu0828@users.noreply.github.com>
2024-04-29 11:24:26 +0800

    Create ardunio.c

---
+++ Inclass/0429_node-red/ardunio.c
@@ -0,0 +1,34 @@
+#include "DHT.h"
+#include<Servo.h>
+#include <Ultrasonic.h>
+Servo myservo;
+DHT dht1(2, DHT11);
+String Str01="c";
+Ultrasonic ultrasonic_2_3(4, 5);  
+
+void setup()
+{
+  dht1.begin();
+  Serial.begin(115200);
+}
+
+void loop()
+{
+  float distance = ultrasonic_2_3.convert(ultrasonic_2_3.timing(), Ultrasonic::CM);
+  if(Serial.available()){
+    delay(1);
+    while(Serial.available()){
+      Str01=(char)Serial.read();
+    }
+    //Serial.println(Str01);
+  }
+
+  String hum,t_c,t_f;
+  hum = dht1.readHumidity();
+  t_c = dht1.readTemperature();
+  t_f = dht1.readTemperature(true);
+  String text = "相對溼度 : " + hum + "," + "攝氏溫度 : " + t_c +  " , " + "華氏溫度 : " + t_f + "," + distance +',';
+  Serial.println(text);
+
+  Str01[0] = 'c';
+}
